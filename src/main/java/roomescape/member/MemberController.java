a191b707cd8dd945e31b992230e9f41c6ef2b643
misim3 <sim000314@naver.com>
2024-09-26 20:44:45 +0900

    feat: 로그인 API 개발

--- src/main/java/roomescape/member/MemberController.java
+++ src/main/java/roomescape/member/MemberController.java
@@ -10,13 +10,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import roomescape.auth.AuthorizationProvider;
+import roomescape.auth.MemberAuthorization;
 
 @RestController
 public class MemberController {
-    private MemberService memberService;
+    private final MemberService memberService;
+    private final AuthorizationProvider authorizationProvider;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, AuthorizationProvider authorizationProvider) {
         this.memberService = memberService;
+        this.authorizationProvider = authorizationProvider;
     }
 
     @PostMapping("/members")
@@ -24,6 +28,16 @@ public class MemberController {
         MemberResponse member = memberService.createMember(memberRequest);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).body(member);
     }
+
+    @PostMapping("/login")
+    public void login(@RequestBody MemberLoginRequest memberLoginRequest, HttpServletResponse response) {
+        MemberAuthorization memberAuthorization = authorizationProvider.createByPayload(memberLoginRequest.email());
+        Cookie cookie = new Cookie("token", memberAuthorization.authorization());
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
 
     @PostMapping("/logout")
     public ResponseEntity logout(HttpServletResponse response) {
